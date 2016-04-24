aa3cd401b845ca7fabfd75837b9466771c87f374
Alexander Neumann <alexander@bumpern.de>
2016-04-24 16:34:34 +0200

    Recover gracefully from panics

--- src/ghenga/server/handlers.go
+++ src/ghenga/server/handlers.go
@@ -4,6 +4,7 @@ package server
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 
@@ -18,10 +19,14 @@ type Env struct {
 	Debug      bool
 }
 
+// HandleFunc is a function similar to http.HandleFunc, but extended by an
+// explicit environment parameter. It may return an error.
+type HandleFunc func(*Env, http.ResponseWriter, *http.Request) error
+
 // Handler is an http.Handler with an explicit error return value, bundled together with an environment.
 type Handler struct {
-	Env        *Env
-	HandleFunc func(*Env, http.ResponseWriter, *http.Request) error
+	Env *Env
+	H   HandleFunc
 }
 
 // httpWriteJSON encodes the given structures as JSON and writes them to the
@@ -35,33 +40,60 @@ func httpWriteJSON(wr http.ResponseWriter, status int, data interface{}) error {
 	return enc.Encode(data)
 }
 
+// jsonError is the struct for an error message returned by the API server.
+type jsonError struct {
+	Message string `json:"message,omitempty"`
+}
+
 // ServeHTTP allows the handler to be used in place of http.Handler.
 func (h Handler) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
-	err := h.HandleFunc(h.Env, wr, req)
+	err := RecoverHandler(h.Env, wr, req, h.H)
 	if err != nil {
 		switch e := err.(type) {
 		case Error:
 			// return the error to the client as a nicely formatted json document.
-
-			type jsonError struct {
-				Error string `json:"error"`
-			}
-
-			err = httpWriteJSON(wr, e.Status(), jsonError{Error: e.Error()})
+			err = httpWriteJSON(wr, e.Status(), jsonError{Message: e.Error()})
 			if err != nil {
 				log.Printf("error writing error document to client: %v", err)
 			}
 		default:
-			log.Printf("unhandled error: %v", err)
+			log.Printf("unhandled error: %#v", err)
+			je := jsonError{Message: "internal server error"}
+
 			if h.Env.Debug {
-				log.Printf("sending verbose message")
-				// return verbose error message
-				http.Error(wr, err.Error(), http.StatusInternalServerError)
-				return
+				je.Message = e.Error()
 			}
 
-			// return a generic internal server error message with status 500
-			http.Error(wr, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			err = httpWriteJSON(wr, http.StatusInternalServerError, je)
+			if err != nil {
+				log.Printf("error writing error document to client: %v", err)
+			}
+			return
 		}
 	}
+}
+
+// RecoverHandler recovers gracefully from panics that occur when running h.
+func RecoverHandler(env *Env, wr http.ResponseWriter, req *http.Request, h HandleFunc) (err error) {
+	defer func() {
+		// catch panic that may have occurred while running the handler
+		if r := recover(); r != nil {
+			log.Printf("panic received!")
+			log.Printf("r: %v", r)
+
+			e := StatusError{Code: http.StatusInternalServerError}
+			switch t := r.(type) {
+			case string:
+				e.Err = errors.New(t)
+			case error:
+				e.Err = t
+			default:
+				e.Err = errors.New("Unknown error")
+			}
+
+			err = e
+		}
+	}()
+
+	return h(env, wr, req)
 }
