4430016d61d4732ba0d0c4a90ff9971ecf622f3b
Alexander Neumann <alexander@bumpern.de>
2016-04-24 20:10:11 +0200

    Fix test

--- src/ghenga/db/helpers_test.go
+++ src/ghenga/db/helpers_test.go
@@ -3,7 +3,12 @@ package db
 import "testing"
 
 func TestNewFakePerson(t *testing.T) {
-	// p, err := NewFakePerson("de")
-	// if err !=
-	t.Fatal("test")
+	p, err := NewFakePerson("de")
+	if err != nil {
+		t.Fatalf("NewFakePerson(): %v", err)
+	}
+
+	if err = p.Validate(); err != nil {
+		t.Fatalf("NewFakePerson() not valid: %v", err)
+	}
 }
